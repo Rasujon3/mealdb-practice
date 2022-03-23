3582dfbe4030f10bd72485577b60845693deb0eb
Rasujon3 <rasujon3@gmail.com>
2022-03-24 02:09:27 +0600

    checklist 1,3,5 done

--- src/components/Meals/Meals.jsx
+++ src/components/Meals/Meals.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Meal from "../Meal/Meal";
+import "./Meals.css";
 
 const Meals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?f=a`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setMeals(data.meals));
+  }, []);
+
   return (
-    <div>
-      <h1>Meals</h1>
+    <div className="shop-container">
+      <div className="products-container">
+        {/* <h1>meals: {meals.length}</h1> */}
+        {meals.map((meal) => (
+          <Meal
+            key={meal.idMeal}
+            product={meal}
+            // handleAddToCart={handleAddToCart}
+          />
+        ))}
+      </div>
+      <div className="cart-container">
+        {/* <Cart cart={cart} /> */}
+        <h1>Cart Summary</h1>
+      </div>
     </div>
   );
 };
